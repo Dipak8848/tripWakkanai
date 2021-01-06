482a07ea95aa12956f830d556dea407b5517d2f6
Dipak8848 <khatridipak11111@gmail.com>
2021-01-06 12:30:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Tripist 
+![nazar (1)](https://user-images.githubusercontent.com/61140841/99997077-b26d8680-2dcd-11eb-9bd8-e95e04b22a5f.gif)
+
+
+Eine mobile Reiseführer-App für Istanbul
+## Motivation
+Bevor wir uns auf eine Reise begeben, gibt es hierzu viele Websites, auf denen wir Informationen im Internet hierzu finden können, aber es erfordert viel Recherche. Eine Mobile Anwendung, mit der wir diese Informationen einfacher und vollständiger erhalten können, ist für alle nützlicher. Ein solcher Reiseführer ist in einer Stadt wie Istanbul, die überfüllt ist und viele Touristen anzieht, unverzichtbar. Nicht nur die Touristen, sondern auch die in Istanbul lebenden Menschen können aufgrund der geschäftigen Arbeitszeiten keine Zeit finden, genügend Nachforschungen anzustellen. Wenn wir daran denken, einen bestimmten Stadtteil zu besuchen, hielten wir es daher für sehr vorteilhaft, dass es sehr nützlich ist, eine Mobile Anwendung zu haben, in der wir unsere Zeit gut verbringen, die uns darüber informiert, was wir dort tun sollen.
+
+## Projektmanagement
+| Abteilung | Abteilungsleiter |
+| --- | --- |
+| Frontend | Büşra Sarı **(Teamsprecher)** |
+| Backend | Tuğba Öğünçmert, Kemal Berke Ak |
+| Dokumentation | Şevval Arslan |
+| Tester | Asım Tatar |
+
+**Unsere Sprint Plan : https://docs.google.com/spreadsheets/d/1fdpTwONh6p4itfnS9OBl6klZ2XE_dcycAqip9KCnJ-o/edit#gid=1269452056**
+
+## Verwendete Werkzeuge
+### Programmierung
+* Sprache: Java
+* Platform: Android Studio
+### Database
+* SQLite
+
+### Koordination
+* Github
+* Google Docs 
+* Google Slides
+* Google Sheets
+
+### GUI Entwurf
+* Adobe XD
+
+### Kommunikation
+* Discord
+* Google Meet 
+* Whatsapp
+
+### Quelle
+#### Icons
+* Flaticon
+* OpenWeather
+#### Illustrations
+* Freepik by Stories
+#### Fotos
+* Shutterstock
+* Pexels
+* Pixabays
+
+## Projektmanagement
+| Abteilung | Abteilungsleiter |
+| --- | --- |
+| Frontend | Büşra Sarı **(Teamsprecher)** |
+| Backend | Tuğba Öğünçmert, Kemal Berke Ak |
+| Dokumentation | Şevval Arslan |
+| Tester | Asım Tatar |
+
+# GUI ENTWURF
+![logom](https://user-images.githubusercontent.com/61140841/99084270-ca782580-25d7-11eb-8216-f9d90ce363f5.png)
+![Google Pixel 4, 4XL – 18](https://user-images.githubusercontent.com/61140841/99084329-db289b80-25d7-11eb-8eac-7ed9b8f8bb87.png)
+![Google Pixel 4, 4XL – 19](https://user-images.githubusercontent.com/61140841/99084337-dcf25f00-25d7-11eb-97f8-3d8d7fcf1ae8.png)
+![Google Pixel 4, 4XL – 22](https://user-images.githubusercontent.com/61140841/99084344-de238c00-25d7-11eb-92d5-c3453d1673cb.png)
+![Google Pixel 4, 4XL – 23](https://user-images.githubusercontent.com/61140841/99084349-df54b900-25d7-11eb-91eb-e3f5eccb1416.png)
+![Google Pixel 4, 4XL – 25](https://user-images.githubusercontent.com/61140841/99084363-e54a9a00-25d7-11eb-9640-cf5870c71601.png)
+
+
+**Der Link zum vollständigen GUI Entwurf: https://xd.adobe.com/view/00c0ff30-336f-4f6d-b482-dab8d8e083fd-5593/**
---
+++ app/src/main/java/com/example/tripwakkanai/MainActivity.java
@@ -0,0 +1,180 @@
+package com.example.tripwakkanai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.tripwakkanai.maps.Bazaar_Markets;
+import com.example.tripwakkanai.maps.Historical_Places;
+import com.example.tripwakkanai.maps.Island_Beachs;
+import com.example.tripwakkanai.maps.Museums;
+
+import com.example.tripwakkanai.maps.My_Locations;
+import com.example.tripwakkanai.maps.Parks;
+import com.example.tripwakkanai.maps.Religions;
+import com.example.tripwakkanai.maps.Squares;
+import com.example.tripwakkanai.ui.home.HomeFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIMER = 5000;
+    SQLiteDatabase database;
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+    SharedPreferences onBoardingScreen;
+    HomeFragment homeFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.imageView2);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                onBoardingScreen = getSharedPreferences("onBoardingScreen", MODE_PRIVATE);
+                boolean isFirstTime = onBoardingScreen.getBoolean("firstTime", true);
+
+                if(isFirstTime){
+
+                    SharedPreferences.Editor editor = onBoardingScreen.edit();
+                    editor.putBoolean("firstTime", false);
+                    editor.commit();
+                    Intent intent = new Intent(getApplicationContext(), OnBoarding.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Intent intent = new Intent(getApplicationContext(), BottomNav.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+
+            }
+        }, SPLASH_TIMER);
+
+
+
+        //create table
+        database = MainActivity.this.openOrCreateDatabase("Places", MODE_PRIVATE, null);
+        Database_Connection.databaseprepare(database);
+
+        //add data
+        Database_Connection.add_religions(database);
+        Database_Connection.add_bazaarmarkets(database);
+        Database_Connection.add_historicalplaces(database);
+        //Database_Connection.add_islandsandbeachs(database);
+        Database_Connection.add_museums(database);
+        Database_Connection.add_myfavourites(database);
+        Database_Connection.add_mylocations(database);
+        Database_Connection.add_parks(database);
+        Database_Connection.add_squares(database);
+        Database_Connection.add_food(database);
+    }
+
+    public void showIsland_Beachs(View view){
+
+        Intent intent = new Intent(this, Island_Beachs.class);
+        intent.putExtra("info","new");
+        startActivity(intent);
+    }
+
+    public void showMy_Locations(View view){
+
+        Intent intent = new Intent(this, My_Locations.class);
+        intent.putExtra("info","new");
+        startActivity(intent);
+
+    }
+
+
+
+    public void showBazaar_Markets(View view){
+        Intent intent = new Intent(this, Bazaar_Markets.class);
+        intent.putExtra("info","new");
+        startActivity(intent);
+
+    }
+
+    public void showHistorical_Places(View view){
+        Intent intent = new Intent(this, Historical_Places.class);
+        intent.putExtra("info","new");
+        startActivity(intent);
+
+    }
+    public void showMuseums(View view){
+        Intent intent = new Intent(this, Museums.class);
+        intent.putExtra("info","new");
+        startActivity(intent);
+
+    }
+    public void showParks(View view){
+        Intent intent = new Intent(this, Parks.class);
+        intent.putExtra("info","new");
+        startActivity(intent);
+
+    }
+    public void showReligions(View view){
+        Intent intent = new Intent(this, Religions.class);
+        intent.putExtra("info","new");
+        startActivity(intent);
+
+    }
+    public void showSquares(View view){
+        Intent intent = new Intent(this, Squares.class);
+        intent.putExtra("info","new");
+        startActivity(intent);
+
+    }
+
+    public void showMyLocationsPage(View view){
+
+        Intent intent = new Intent(this,MyLocationsPage.class);
+
+        startActivity(intent);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
